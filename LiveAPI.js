cf5d0378fbe4ed67bbc4cf70e7f288c00214ec66
KSMurph <kennanmurphy@gmail.com>
2018-04-24 11:39:56 -0400

    Update LiveAPI.js

--- LiveAPI.js
+++ LiveAPI.js
@@ -34,6 +34,7 @@ function renderPage(answer) {
   return;
 }
 
+// This format is US specific, feel free to replace with your preferred format
 function formatPhoneNumber(s) {
   var s2 = (""+s).replace(/\D/g, '');
   var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
